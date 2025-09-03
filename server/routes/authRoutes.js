a76d5a6b5156f3a54a1282b97583fe56e2312e8f
Dawaman43 <Dawitworkujima@gmail.com>
2025-09-03 15:20:45 +0300

    corrected admin user add functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { adminRegister, login, logout } from "../controllers/authController.js";
-import { verifyToken } from "../middlewares/auth.js";
+import { isAdmin, verifyToken } from "../middlewares/auth.js";
 
 const route = express.Router();
 
-route.post("/admin-register", adminRegister);
 route.post("/login", login);
 
 //user must be logged in to logout
 route.post("/logout", verifyToken, logout);
+
+//admin register
+route.post("/admin-register", verifyToken, isAdmin, adminRegister);
 
 export default route;
