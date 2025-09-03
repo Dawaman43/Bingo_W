4578f6d6c639d13ec6667585cac520d20e732ed9
Dawaman43 <Dawitworkujima@gmail.com>
2025-09-03 14:40:47 +0300

    corrected the ex module name conflict

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { addUser } from "../controllers/adminController";
-import { isAdmin } from "../middlewares/auth";
+import { addUser } from "../controllers/adminController.js";
+import { isAdmin } from "../middlewares/auth.js";
 const route = express.Router();
 
 route.post("/add-user", isAdmin, addUser);
