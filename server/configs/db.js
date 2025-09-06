5931979c4fa3142fd170b2f0cbde8ca11e6db415
Chala Temesgen <ctemesgen85@gmail.com>
2025-09-06 17:09:10 -0300

    some updates in the configs

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected'))
-        await mongoose.connect(`${process.env.MONGODB_URI}/jobs-park`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/bingo-web`)
     } catch (error) {
         console.log(error.message);
     }
